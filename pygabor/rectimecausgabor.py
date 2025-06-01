a1f40fa1dfd64387953f311b9d9d101083e2f623
stivenschwanz <stivendias@gmail.com>
2025-06-01 23:45:49 +0000

    Improving spectogram

--- pygabor/rectimecausgabor.py
+++ pygabor/rectimecausgabor.py
@@ -2,6 +2,7 @@
 """
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class RecursiveTimeCausGaborMethod:
@@ -16,54 +17,58 @@ class RecursiveTimeCausGaborMethod:
     level_prev : np.ndarray
     n : int
 
-    def __init__(self, omega, delta_t = 1.0, c = 2.0, numlevels = 8, N = 1.0):
+    def __init__(self, omega, delta_t = 1.0, c = 2.0, numlevels = 16, N = 1.0):
 
         # Distribution parameter
         self.c = c
 
-        assert c > 1
+        assert self.c > 1
 
         # Number of temporal scale levels
         self.K = numlevels
 
-        assert numlevels > 1
+        assert self.K > 1
 
         # Proportionality factor
         self.N = N
 
-        assert N >= 1
+        assert self.N >= 1
 
         # Angular frequencies [rad/s]
-        self.omega = omega
+        self.omega = np.array(omega)
 
-        self.J = np.size(omega)
+        self.J = np.size(self.omega)
 
         assert self.J > 1
 
+        self.frequencies = self.omega / (2*np.pi)
+
         # Initialize the output buffers
-        self.level = np.zeros((J, K, 2))
-        self.level_prev = np.zeros((J, K, 2))
+        self.level = np.zeros((self.J, self.K, 2))
+        self.level_prev = np.zeros((self.J, self.K, 2))
 
         # Sampling time [s]
         self.delta_t = delta_t
 
-        assert delta_t > 0
+        assert self.delta_t > 0
+
+        self.duration = self.K * self.delta_t
 
         # Sampling rate [Hz]
         r = 1.0/delta_t
 
         # Initialize the time constants
-        self.mu = np.zeros((J, K))
-        self.gain = np.zeros((J, K))
+        self.mu = np.zeros((self.J, self.K))
+        self.gain = np.zeros((self.J, self.K))
 
-        for j in range(J):
+        for j in range(self.J):
             sigma_j0 = 2*np.pi*N/omega[j]
             tau_jref = np.pow(r * sigma_j0, 2)
 
             tau_jk_1 = 0
-            for k in range(K):
+            for k in range(self.K):
                 # Compute the temporal scale levels
-                tau_jk = np.pow(c, 2*k-K) * tau_jref
+                tau_jk = np.pow(c, 2*(k-self.K)) * tau_jref
 
                 # Compute the temporal scale increments
                 delta_tau_jk = tau_jk - tau_jk_1
@@ -77,18 +82,42 @@ class RecursiveTimeCausGaborMethod:
 
         self.n = 0
     
-    def process(signal):
-        for j in range(J):
-            t = n * delta_t
-            omega_jt = omega[j] * t
+    def process(self, signal):
+        for j in range(self.J):
+            t = self.n * self.delta_t
+            omega_jt = self.omega[j] * t
             input = np.array([signal * np.cos(omega_jt), -signal * np.sin(omega_jt)])
 
             aux = input
-            for k in range(K):
-                level[j,k,:] = aux = level_prev[j,k,:] + gain[j,k] * (aux - level_prev[j,k,:])
+            for k in range(self.K):
+                self.level[j,k,:] = self.level_prev[j,k,:] + self.gain[j,k] * (aux - self.level_prev[j,k,:])
+                aux = self.level[j,k,:]
         
-        level_prev = level
+        self.level_prev = self.level.copy()
 
-        n += 1
+        self.n += 1
 
-        return level
+        return self.level
+
+    def spectrogram(self, lowsoftthresh : float = 0.000001, maxrange : float = 60):
+
+        # Compute the absolute value of the spectrogram
+        absspectrogram = np.sqrt(self.level[:,:,0]**2 + self.level[:,:,1]**2)
+
+        # Compute logarithmic magnitudes in dB, with additional lower bound
+        maxval = np.max(absspectrogram)
+        logspectrogram = 20 * np.log10(absspectrogram/maxval + lowsoftthresh)
+        logspectrogram[logspectrogram < -maxrange] = -maxrange
+
+        im = \
+        plt.imshow(logspectrogram, \
+                    cmap='jet', interpolation='nearest', aspect='auto', \
+                    origin='lower', \
+                    extent=[0, self.duration, min(self.frequencies), max(self.frequencies)])
+        plt.colorbar(im)
+
+        plt.xlabel("Time (seconds)")
+        plt.ylabel("Log Frequency (Hz)")
+        plt.show(block=False)
+
+        return logspectrogram
